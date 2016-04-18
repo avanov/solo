84750ad8131e3687de96b40accaa730fdfa4d015
Maxim Avanov <maxim.avanov@gmail.com>
2016-04-18 08:32:51 +0300

    Work in progress on accounts

--- solo/apps/accounts/models.py
+++ solo/apps/accounts/models.py
@@ -1,11 +1,28 @@
+import enum
 import sqlalchemy as sa
 
 from solo.server.model import Base
+from solo.server.db.types import PythonMappedEnum
 from solo.configurator.models import register_model
+
+
+class AuthProvider(enum.Enum):
+    EMAIL = 'email'
+    GITHUB = 'github'
+
 
 
 @register_model(category='users')
 class User(Base):
     __tablename__ = 'users'
 
+    id = sa.Column(sa.Integer, primary_key=True)
+
+
+class Auth(Base):
+    __tablename__ = 'auth'
+
     id = sa.Column(sa.BigInteger, primary_key=True)
+    provider = sa.Column(PythonMappedEnum(AuthProvider), nullable=False)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), nullable=False, index=True)
+    auth_key = sa.Column(sa.Unicode(140), nullable=False, default='', server_default='', index=True)
---
+++ solo/server/db/types.py
@@ -0,0 +1,36 @@
+import logging
+import sqlalchemy.types as sa_types
+from enum import Enum as PythonEnum
+
+
+log = logging.getLogger(__name__)
+
+
+class PythonMappedEnum(sa_types.TypeDecorator):
+    """ Implements mapping between Postgres' Enums and Python Enums.
+    """
+    impl = sa_types.Enum
+
+    def __init__(self, python_enum_type: PythonEnum, **kwargs):
+        self.python_enum_type = python_enum_type
+        self.kwargs = kwargs
+        enum_args = [x.value for x in python_enum_type]
+        super(PythonMappedEnum, self).__init__(*enum_args, **self.kwargs)
+
+    def process_bind_param(self, value: PythonEnum, dialect):
+        """ Convert to postgres value
+        """
+        return value.value
+
+    def process_result_value(self, value: str, dialect):
+        """ Convert to python value
+        """
+        for __, case in self.python_enum_type.__members__.items():
+            if case.value == value:
+                return case
+        raise TypeError("Cannot map Enum value '{}' to Python's {}".format(
+            value, self.python_enum_type
+        ))
+
+    def copy(self):
+        return PythonMappedEnum(self.python_enum_type, **self.kwargs)
