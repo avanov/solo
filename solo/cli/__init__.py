b2d633f8e27c38c61adadd55185cd0ff09a000e1
Maxim Avanov <maxim.avanov@gmail.com>
2016-05-18 15:32:53 +0300

    Reorganise CLI main entry point

--- solo/cli/__init__.py
+++ solo/cli/__init__.py
@@ -14,32 +14,11 @@ import yaml
 from solo.server import init_webapp
 
 
-def main(args=None, stdout=None):
-    cli_parser = argparse.ArgumentParser(description='Manage solo projects.')
-    cli_parser.add_argument('command', help="Solo command")
-    cli_parser.add_argument('cmd_options', type=str, nargs='*',
-                            help='command-specific options')
-    cli_parser.add_argument('-V', '--version', action='version',
-                            version='Solo {}'.format(get_distribution("solo").version))
-
-    if args is None:
-        args = sys.argv[1:]
-    args = cli_parser.parse_args(args)
-    try:
-        cmd = CLI_COMMANDS[args.command]
-    except KeyError:
-        sys.exit('Solo command "{cmd}" was not found. Use one of the following: \n- {available}'.format(
-            cmd=args.command,
-            available='\n- '.join([c for c in CLI_COMMANDS.keys()])
-        ))
-
-    cmd(args.cmd_options)
-
-
-def run_cmd(args: List[str]):
+def run_cmd(args: argparse.Namespace):
     """ Run project instance.
     """
-    config = parse_app_config(args)
+    with Path(args.config).open() as f:
+        config = yaml.load(f.read())
 
     logging.config.dictConfig(config['logging'])
     log = logging.getLogger('solo')
@@ -60,20 +39,40 @@ def run_cmd(args: List[str]):
     sys.exit(0)
 
 
+def db_cmd(args: argparse.Namespace):
+    pass
+
+
+def main(args=None, stdout=None):
+    parser = argparse.ArgumentParser(description='Manage solo projects.')
+    parser.add_argument('-V', '--version', action='version',
+                        version='Solo {}'.format(get_distribution("solo").version))
+    subparsers = parser.add_subparsers(title='sub-commands',
+                                       description='valid sub-commands',
+                                       help='additional help')
+
+    # $ solo run <config>
+    # ---------------------------
+    run = subparsers.add_parser('run', help='Run solo application')
+    run.add_argument("config", help="path to a YAML config.")
+    run.set_defaults(func=run_cmd)
+
+    # $ solo db [args]
+    # ---------------------------
+    db = subparsers.add_parser('db', help='Database commands integrated with Alembic CLI')
+    db.add_argument('alembic_cmd', type=str, nargs='*',
+                    help='alembic-specific command')
+    db.set_defaults(func=db_cmd)
+
+    # Parse
+    # ---------------------------
+    if args is None:
+        args = sys.argv[1:]
+    args = parser.parse_args(args)
+    args.func(args)
+
+
+
 INIT_CONFIG = """---
 # Solo App Config
 """
-
-
-CLI_COMMANDS = {
-    'run': run_cmd,
-}
-
-
-def parse_app_config(argv: List[str]) -> Dict[str, Any]:
-    p = argparse.ArgumentParser()
-    p.add_argument("config", help="path to a YAML config.")
-    args = p.parse_args(argv)
-    with Path(args.config).open() as f:
-        config = yaml.load(f.read())
-    return config
