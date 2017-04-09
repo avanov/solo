aa35cd7f3a00a534215dc99a9a34a3a51f058d87
Maxim Avanov <maxim.avanov@gmail.com>
2017-04-09 16:13:39 +0100

    Update dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,289 +1,163 @@
-""" https://pytest.org/latest/fixture.html#sharing-a-fixture-across-tests-in-a-module-or-class-session
+# """ https://pytest.org/latest/fixture.html#sharing-a-fixture-across-tests-in-a-module-or-class-session
+#
+# This conftest is a copy of https://github.com/KeepSafe/aiohttp/blob/master/tests/conftest.py
+#
+# Since there's no a working test suite for aiohttp (https://github.com/sloria/webtest-aiohttp/issues/1),
+# we use the same suite as the base framework.
+# """
+# import asyncio
+# import aiohttp
+# import collections
+# import logging
+# import pytest
+# import re
+# import sys
+# import warnings
+#
+# from solo.testutils import (
+#     loop_context, unused_port
+# )
+#
+# from solo.cli.util import parse_app_config
+# from solo import init_webapp
+#
+#
 
-This conftest is a copy of https://github.com/KeepSafe/aiohttp/blob/master/tests/conftest.py
-
-Since there's no a working test suite for aiohttp (https://github.com/sloria/webtest-aiohttp/issues/1),
-we use the same suite as the base framework.
-"""
-import asyncio
-import aiohttp
-import collections
-import logging
+#
+# @pytest.yield_fixture
+# def create_server(loop):
+#     app = handler = srv = None
+#     config = parse_app_config('./test_config.yml')  # TODO: move path to outer scope
+#
+#     async def create(*, debug=False, ssl_ctx=None, proto='http'):
+#         nonlocal app, handler, srv, config
+#         port = unused_port()
+#         app_manager = await init_webapp(loop, config)
+#         server_future = app_manager.create_server_future(host='127.0.0.1', port=port, ssl=ssl_ctx)
+#         if ssl_ctx:
+#             proto += 's'
+#         url = "{}://127.0.0.1:{}/".format(proto, port)
+#
+#         app = app_manager.app
+#         handler = app_manager.handler
+#         srv = await server_future
+#         return app, url
+#
+#     yield create
+#
+#
+#     async def finish():
+#         if hasattr(app, 'dbengine'):
+#             app.dbengine.terminate()
+#             await app.dbengine.wait_closed()
+#
+#         if hasattr(app, 'memstore'):
+#             await app.memstore.clear()
+#
+#         await handler.finish_connections()
+#         await app.finish()
+#         srv.close()
+#         await srv.wait_closed()
+#
+#     loop.run_until_complete(finish())
+#
+#
+# class Client:
+#     def __init__(self, session, url):
+#         self._session = session
+#         self._url = url
+#
+#     def close(self):
+#         self._session.close()
+#
+#     def get(self, path, **kwargs):
+#         url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
+#         return self._session.get(url, **kwargs)
+#
+#     def post(self, path, **kwargs):
+#         url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
+#         return self._session.post(url, **kwargs)
+#
+#     def delete(self, path, **kwargs):
+#         url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
+#         return self._session.delete(url)
+#
+#     def ws_connect(self, path, **kwargs):
+#         url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
+#         return self._session.ws_connect(url, **kwargs)
+#
+#
+# @pytest.yield_fixture
+# def create_app_and_client(create_server, loop):
+#     client = None
+#
+#     @asyncio.coroutine
+#     def maker(*, server_params=None, client_params=None):
+#         nonlocal client
+#         if server_params is None:
+#             server_params = {}
+#         server_params.setdefault('debug', False)
+#         server_params.setdefault('ssl_ctx', None)
+#         app, url = yield from create_server(**server_params)
+#         if client_params is None:
+#             client_params = {}
+#         client = Client(aiohttp.ClientSession(loop=loop, **client_params), url)
+#         return app, client
+#
+#     yield maker
+#     client.close()
+#
+#
+# @pytest.mark.tryfirst
+# def pytest_pycollect_makeitem(collector, name, obj):
+#     if collector.funcnamefilter(name):
+#         if not callable(obj):
+#             return
+#         item = pytest.Function(name, parent=collector)
+#         if 'run_loop' in item.keywords:
+#             return list(collector._genfunctions(name, obj))
+#
+#
+# @pytest.mark.tryfirst
+# def pytest_pyfunc_call(pyfuncitem):
+#     """
+#     Run asyncio marked test functions in an event loop instead of a normal
+#     function call.
+#     """
+#     if 'run_loop' in pyfuncitem.keywords:
+#         funcargs = pyfuncitem.funcargs
+#         loop = funcargs['loop']
+#         testargs = {arg: funcargs[arg]
+#                     for arg in pyfuncitem._fixtureinfo.argnames}
+#         loop.run_until_complete(pyfuncitem.obj(**testargs))
+#         return True
+#
+#
+# def pytest_runtest_setup(item):
+#     if 'run_loop' in item.keywords and 'loop' not in item.fixturenames:
+#         # inject an event loop fixture for all async tests
+#         item.fixturenames.append('loop')
+#
+#
+# def pytest_ignore_collect(path, config):
+#     if 'test_py35' in str(path):
+#         if sys.version_info < (3, 5, 0):
+#             return True
 import pytest
-import re
-import sys
-import warnings
-
 from aiohttp import web
-from solo.testutils import (
-    loop_context, unused_port
-)
 
 from solo.cli.util import parse_app_config
 from solo import init_webapp
 
 
-class _AssertWarnsContext:
-    """A context manager used to implement TestCase.assertWarns* methods."""
-
-    def __init__(self, expected, expected_regex=None):
-        self.expected = expected
-        if expected_regex is not None:
-            expected_regex = re.compile(expected_regex)
-        self.expected_regex = expected_regex
-        self.obj_name = None
-
-    def __enter__(self):
-        # The __warningregistry__'s need to be in a pristine state for tests
-        # to work properly.
-        for v in sys.modules.values():
-            if getattr(v, '__warningregistry__', None):
-                v.__warningregistry__ = {}
-        self.warnings_manager = warnings.catch_warnings(record=True)
-        self.warnings = self.warnings_manager.__enter__()
-        warnings.simplefilter("always", self.expected)
-        return self
-
-    def __exit__(self, exc_type, exc_value, tb):
-        self.warnings_manager.__exit__(exc_type, exc_value, tb)
-        if exc_type is not None:
-            # let unexpected exceptions pass through
-            return
-        try:
-            exc_name = self.expected.__name__
-        except AttributeError:
-            exc_name = str(self.expected)
-        first_matching = None
-        for m in self.warnings:
-            w = m.message
-            if not isinstance(w, self.expected):
-                continue
-            if first_matching is None:
-                first_matching = w
-            if (self.expected_regex is not None and
-                    not self.expected_regex.search(str(w))):
-                continue
-            # store warning for later retrieval
-            self.warning = w
-            self.filename = m.filename
-            self.lineno = m.lineno
-            return
-        # Now we simply try to choose a helpful failure message
-        if first_matching is not None:
-            __tracebackhide__ = True
-            assert 0, '"{}" does not match "{}"'.format(
-                self.expected_regex.pattern, str(first_matching))
-        if self.obj_name:
-            __tracebackhide__ = True
-            assert 0, "{} not triggered by {}".format(exc_name,
-                                                      self.obj_name)
-        else:
-            __tracebackhide__ = True
-            assert 0, "{} not triggered".format(exc_name)
-
-
-_LoggingWatcher = collections.namedtuple("_LoggingWatcher",
-                                         ["records", "output"])
-
-
-class _CapturingHandler(logging.Handler):
-    """
-    A logging handler capturing all (raw and formatted) logging output.
-    """
-
-    def __init__(self):
-        logging.Handler.__init__(self)
-        self.watcher = _LoggingWatcher([], [])
-
-    def flush(self):
-        pass
-
-    def emit(self, record):
-        self.watcher.records.append(record)
-        msg = self.format(record)
-        self.watcher.output.append(msg)
-
-
-class _AssertLogsContext:
-    """A context manager used to implement TestCase.assertLogs()."""
-
-    LOGGING_FORMAT = "%(levelname)s:%(name)s:%(message)s"
-
-    def __init__(self, logger_name=None, level=None):
-        self.logger_name = logger_name
-        if level:
-            self.level = logging._nameToLevel.get(level, level)
-        else:
-            self.level = logging.INFO
-        self.msg = None
-
-    def __enter__(self):
-        if isinstance(self.logger_name, logging.Logger):
-            logger = self.logger = self.logger_name
-        else:
-            logger = self.logger = logging.getLogger(self.logger_name)
-        formatter = logging.Formatter(self.LOGGING_FORMAT)
-        handler = _CapturingHandler()
-        handler.setFormatter(formatter)
-        self.watcher = handler.watcher
-        self.old_handlers = logger.handlers[:]
-        self.old_level = logger.level
-        self.old_propagate = logger.propagate
-        logger.handlers = [handler]
-        logger.setLevel(self.level)
-        logger.propagate = False
-        return handler.watcher
-
-    def __exit__(self, exc_type, exc_value, tb):
-        self.logger.handlers = self.old_handlers
-        self.logger.propagate = self.old_propagate
-        self.logger.setLevel(self.old_level)
-        if exc_type is not None:
-            # let unexpected exceptions pass through
-            return False
-        if len(self.watcher.records) == 0:
-            __tracebackhide__ = True
-            assert 0, ("no logs of level {} or higher triggered on {}"
-                       .format(logging.getLevelName(self.level),
-                               self.logger.name))
-
-
-@pytest.yield_fixture
-def warning():
-    yield _AssertWarnsContext
-
-
-@pytest.yield_fixture
-def log():
-    yield _AssertLogsContext
-
-
-# add the unused_port and loop fixtures
-pytest.fixture(unused_port)
-
-
-@pytest.yield_fixture
-def loop():
-    with loop_context() as loop:
-        yield loop
-
-
-@pytest.yield_fixture
-def create_server(loop):
-    app = handler = srv = None
+@pytest.fixture
+def app_config():
     config = parse_app_config('./test_config.yml')  # TODO: move path to outer scope
-
-    async def create(*, debug=False, ssl_ctx=None, proto='http'):
-        nonlocal app, handler, srv, config
-        port = unused_port()
-        app_manager = await init_webapp(loop, config)
-        server_future = app_manager.create_server_future(host='127.0.0.1', port=port, ssl=ssl_ctx)
-        if ssl_ctx:
-            proto += 's'
-        url = "{}://127.0.0.1:{}/".format(proto, port)
-
-        app = app_manager.app
-        handler = app_manager.handler
-        srv = await server_future
-        return app, url
-
-    yield create
+    return config
 
 
-    async def finish():
-        if hasattr(app, 'dbengine'):
-            app.dbengine.terminate()
-            await app.dbengine.wait_closed()
-
-        if hasattr(app, 'memstore'):
-            await app.memstore.clear()
-
-        await handler.finish_connections()
-        await app.finish()
-        srv.close()
-        await srv.wait_closed()
-
-    loop.run_until_complete(finish())
-
-
-class Client:
-    def __init__(self, session, url):
-        self._session = session
-        self._url = url
-
-    def close(self):
-        self._session.close()
-
-    def get(self, path, **kwargs):
-        url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
-        return self._session.get(url, **kwargs)
-
-    def post(self, path, **kwargs):
-        url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
-        return self._session.post(url, **kwargs)
-
-    def delete(self, path, **kwargs):
-        url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
-        return self._session.delete(url)
-
-    def ws_connect(self, path, **kwargs):
-        url = "{}/{}".format(self._url.rstrip('/'), path.lstrip('/'))
-        return self._session.ws_connect(url, **kwargs)
-
-
-@pytest.yield_fixture
-def create_app_and_client(create_server, loop):
-    client = None
-
-    @asyncio.coroutine
-    def maker(*, server_params=None, client_params=None):
-        nonlocal client
-        if server_params is None:
-            server_params = {}
-        server_params.setdefault('debug', False)
-        server_params.setdefault('ssl_ctx', None)
-        app, url = yield from create_server(**server_params)
-        if client_params is None:
-            client_params = {}
-        client = Client(aiohttp.ClientSession(loop=loop, **client_params), url)
-        return app, client
-
-    yield maker
-    client.close()
-
-
-@pytest.mark.tryfirst
-def pytest_pycollect_makeitem(collector, name, obj):
-    if collector.funcnamefilter(name):
-        if not callable(obj):
-            return
-        item = pytest.Function(name, parent=collector)
-        if 'run_loop' in item.keywords:
-            return list(collector._genfunctions(name, obj))
-
-
-@pytest.mark.tryfirst
-def pytest_pyfunc_call(pyfuncitem):
-    """
-    Run asyncio marked test functions in an event loop instead of a normal
-    function call.
-    """
-    if 'run_loop' in pyfuncitem.keywords:
-        funcargs = pyfuncitem.funcargs
-        loop = funcargs['loop']
-        testargs = {arg: funcargs[arg]
-                    for arg in pyfuncitem._fixtureinfo.argnames}
-        loop.run_until_complete(pyfuncitem.obj(**testargs))
-        return True
-
-
-def pytest_runtest_setup(item):
-    if 'run_loop' in item.keywords and 'loop' not in item.fixturenames:
-        # inject an event loop fixture for all async tests
-        item.fixturenames.append('loop')
-
-
-def pytest_ignore_collect(path, config):
-    if 'test_py35' in str(path):
-        if sys.version_info < (3, 5, 0):
-            return True
+@pytest.fixture
+def web_client(loop, test_client, app_config):
+    app_manager = loop.run_until_complete(init_webapp(loop, app_config))
+    app = app_manager.app
+    return loop.run_until_complete(test_client(app))
--- tests/integration/apps/auth/api/test_handlers.py
+++ tests/integration/apps/auth/api/test_handlers.py
@@ -1,16 +1,8 @@
-def test_authentication_handler(loop, create_app_and_client):
-    run = loop.run_until_complete
-    app, client = run(create_app_and_client())
+async def test_url__login__be__github(web_client):
+    resp = await web_client.post("/api/login/github", allow_redirects=False)
+    assert resp.status == 302
 
-    async def test_url__login__be__github():
-        nonlocal client
-        resp = await client.post("/api/login/github", allow_redirects=False)
-        assert resp.status == 302
+async def test_url__login__be__github__callback(web_client):
+    resp = await web_client.get("/api/login/github/callback", allow_redirects=False)
+    assert resp.status == 400
 
-    async def test_url__login__be__github__callback():
-        nonlocal client
-        resp = await client.get("/api/login/github/callback", allow_redirects=False)
-        assert resp.status == 400
-
-    run(test_url__login__be__github())
-    run(test_url__login__be__github__callback())
